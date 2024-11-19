63bce4755f6c89c2309a7cf0667949b25ca3aeaa
Nick <ngstafford1@gmail.com>
2024-11-19 11:27:13 -0500

    tailwind working. addind shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./main.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello World</h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   </StrictMode>,
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-      proxy: {
-        '/api': {
-          target: 'http://127.0.0.1:5000/',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
-
