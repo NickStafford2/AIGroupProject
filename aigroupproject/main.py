c3daa9d440f01ef75fdbd756a8543b24a785e563
= <ngstafford1@gmail.com>
2024-11-19 14:57:21 -0500

    fixed missing file and blueprint route error

--- aigroupproject/main.py
+++ aigroupproject/main.py
@@ -3,6 +3,6 @@ from flask import Blueprint, jsonify, request
 bp = Blueprint("main", __name__)
 
 
-@bp.route("test", methods=["GET"])
+@bp.route("/test", methods=["GET"])
 def test():
     return "success"
