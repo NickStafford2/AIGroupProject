e1ef34d0794eb2c049e02693f85860a933d63160
EanDodge <145071390+EanDodge@users.noreply.github.com>
2024-12-05 11:57:44 -0500

    had to fix circular import

--- aigroupproject/main.py
+++ aigroupproject/main.py
@@ -6,3 +6,23 @@ bp = Blueprint("main", __name__)
 @bp.route("/test", methods=["GET"])
 def test():
     return "success"
+
+# main.py
+from sudoku import Sudoku
+from sudoku_solver import find_list_of_board, solve, Sud
+
+difficulty = float(input("What difficulty would you like the puzzle to be?[0-1]: "))
+puzzle = Sudoku(3).difficulty(difficulty)
+puzzle.show()
+solution = puzzle.solve()
+
+board = find_list_of_board(puzzle.board)
+print("This is the actual solution: ")
+solution.show()
+
+if solve(board):
+    print("Solved Sudoku from AI:")
+    result = Sud(board)
+    print(result)
+else:
+    print("No solution exists.")
